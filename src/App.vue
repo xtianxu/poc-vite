1bda57b81297089db06b7b7d7cd2106948536349
Chris Xu 徐国欣 <xugx14@cn.midea.com>
2021-07-07 16:21:18 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  
+  <SplitPane/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import SplitPane from './components/SplitPane.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    SplitPane
+  }
+}
+</script>
---
+++ src/components/SplitPane.vue
@@ -0,0 +1,51 @@
+<template>
+  <main>
+      <nav>
+
+      </nav>
+      <div class="content">
+          <img alt="Vue logo" src="../assets/logo.png" />
+      </div>
+  </main>
+</template>
+
+<script>
+export default {
+    name: 'HelloWorld',
+    props: {
+        msg: String
+    },
+    data() {
+        return {
+            count: 0
+        }
+    },
+    methods:{
+
+    }
+}
+</script>
+<style lang="stylus" scoped>
+main{
+    display flex
+    width 100%
+    height 100%
+    nav{
+        flex 0 0 auto
+        width 200px
+        background-color red
+    }
+    nav:after{
+        background-color: #ccc;
+        position: absolute;
+        left: 0;
+        width: 4px;
+        height: 100%;
+        cursor: w-resize;
+    }
+    .content{
+        flex 1 1 auto
+        background-color green
+    }
+}
+</style>
